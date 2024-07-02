f760453a573188714e663b7568098da36203519c
Alex Lin <chinyinalex.lin@gmail.com>
2024-07-02 02:34:10 -0400

    routes and component

---
+++ employee-management-app/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { DepartmentListComponent } from './components/department-list/department-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeFormComponent,
+    EmployeeDetailsComponent,
+    LoginComponent,
+    DepartmentListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/employees', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'employees', component: EmployeeListComponent },
+      { path: 'employees/new', component: EmployeeFormComponent },
+      { path: 'employees/:id', component: EmployeeDetailsComponent },
+      { path: 'employees/:id/edit', component: EmployeeFormComponent },
+      { path: 'departments', component: DepartmentListComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- employee-management-app/src/app/app.routes.ts
+++ employee-management-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { DepartmentListComponent } from './components/department-list/department-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'employees/new', component: EmployeeFormComponent },
+  { path: 'employees/:id', component: EmployeeDetailsComponent },
+  { path: 'employees/:id/edit', component: EmployeeFormComponent },
+  { path: 'departments', component: DepartmentListComponent },
+];
