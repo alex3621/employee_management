a31f678f3d3d61999b700636934eff957ae7cfe6
Alex Lin <chinyinalex.lin@gmail.com>
2024-07-02 04:16:51 -0400

    update app component and routing as well as employee list UI

--- employee-management-app_frontend/src/app/app.routes.ts
+++ employee-management-app_frontend/src/app/app.routes.ts
@@ -1,11 +1,12 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
 import { LoginComponent } from './components/login/login.component';
 import { DepartmentListComponent } from './components/department-list/department-list.component';
 
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', redirectTo: '/employees', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'employees', component: EmployeeListComponent },
@@ -14,3 +15,9 @@ export const routes: Routes = [
   { path: 'employees/:id/edit', component: EmployeeFormComponent },
   { path: 'departments', component: DepartmentListComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
