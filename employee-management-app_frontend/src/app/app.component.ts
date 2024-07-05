cb61fe2b14ba70e0982ec5477a914e9c4188d057
Alex Lin <chinyinalex.lin@gmail.com>
2024-07-04 21:47:46 -0400

    fixed bug where the nav bar links in app component isn't working

--- employee-management-app_frontend/src/app/app.component.ts
+++ employee-management-app_frontend/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'employee-management-app';
+  logClick(route: string) {
+    console.log(`Clicked ${route}`);
+  }
 }
