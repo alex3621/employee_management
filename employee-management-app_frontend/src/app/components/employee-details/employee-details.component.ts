318f1e4d7110bb96879d5b20b7f4e8434d9e5190
Alex Lin <chinyinalex.lin@gmail.com>
2024-07-16 22:50:28 -0400

    updated details html and ts file to reflect age gender

--- employee-management-app_frontend/src/app/components/employee-details/employee-details.component.ts
+++ employee-management-app_frontend/src/app/components/employee-details/employee-details.component.ts
@@ -53,6 +53,19 @@ export class EmployeeDetailsComponent implements OnInit {
     });
   }
 
+  calculateAge(birthDate: Date): number {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDiff = today.getMonth() - birthDateObj.getMonth();
+    
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDateObj.getDate())) {
+      age--;
+    }
+    
+    return age;
+  }
+
   goBack(): void {
     this.router.navigate(['/employees']);
   }
