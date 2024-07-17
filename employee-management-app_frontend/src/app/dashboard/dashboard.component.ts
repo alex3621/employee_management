ae818a66a41718dcf16bfede6d2c746c5094eee2
Alex Lin <chinyinalex.lin@gmail.com>
2024-07-16 22:57:08 -0400

    updated dashboard to contain more information as well as updated the styling

--- employee-management-app_frontend/src/app/dashboard/dashboard.component.ts
+++ employee-management-app_frontend/src/app/dashboard/dashboard.component.ts
@@ -1,35 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../services/employee.service';
-import { DepartmentService } from '../services/department.service';
+import { Employee } from '../models/employee.model';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,  
+  imports: [CommonModule], 
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
   totalEmployees: number = 0;
   totalDepartments: number = 0;
+  genderDistribution: { male: number, female: number } = { male: 0, female: 0 };
+  ageDistribution: { '20-30': number, '31-40': number, '41-50': number, '51+': number } = { '20-30': 0, '31-40': 0, '41-50': 0, '51+': 0 };
+  averageTenure: number = 0;
 
-  constructor(
-    private employeeService: EmployeeService,
-    private departmentService: DepartmentService
-  ) {}
+  constructor(private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
-    this.getTotalEmployees();
-    this.getTotalDepartments();
+    this.getEmployeeStats();
   }
 
-  getTotalEmployees(): void {
-    this.employeeService.getEmployees().subscribe(
-      employees => this.totalEmployees = employees.length
-    );
+  getEmployeeStats(): void {
+    this.employeeService.getEmployees().subscribe(employees => {
+      this.totalEmployees = employees.length;
+      this.calculateGenderDistribution(employees);
+      this.calculateAgeDistribution(employees);
+      this.calculateAverageTenure(employees);
+      this.totalDepartments = new Set(employees.map(e => e.department)).size;
+    });
   }
 
-  getTotalDepartments(): void {
-    this.departmentService.getDepartments().subscribe(
-      departments => this.totalDepartments = departments.length
-    );
+  calculateGenderDistribution(employees: Employee[]): void {
+    this.genderDistribution.male = employees.filter(e => e.gender === 'Male').length;
+    this.genderDistribution.female = employees.filter(e => e.gender === 'Female').length;
+  }
+
+  calculateAgeDistribution(employees: Employee[]): void {
+    const currentYear = new Date().getFullYear();
+    employees.forEach(e => {
+      const age = currentYear - e.birthDate.getFullYear();
+      if (age <= 30) this.ageDistribution['20-30']++;
+      else if (age <= 40) this.ageDistribution['31-40']++;
+      else if (age <= 50) this.ageDistribution['41-50']++;
+      else this.ageDistribution['51+']++;
+    });
+  }
+
+  calculateAverageTenure(employees: Employee[]): void {
+    const currentDate = new Date();
+    const totalTenure = employees.reduce((sum, e) => {
+      const tenure = (currentDate.getTime() - e.hireDate.getTime()) / (1000 * 60 * 60 * 24 * 365);
+      return sum + tenure;
+    }, 0);
+    this.averageTenure = totalTenure / employees.length;
   }
 }
